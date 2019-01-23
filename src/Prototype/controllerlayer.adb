87df47ee11c3a2ac7c3e0bf999a1711c0cc6faea
axal25 <emevig@gmail.com>
2019-01-23 03:18:26 +0100

    Working Show Found PossibleMoves

--- src/Prototype/controllerlayer.adb
+++ src/Prototype/controllerlayer.adb
@@ -63,9 +63,10 @@ package body ControllerLayer is
                                                    "clicked", 
                                                    UserCallback_Position.To_Marshaller( Deactivate_Button_Call'Access ),
                                                    aPosition );
-      aAllData.aMainWindow.aWindow.Show_All;
       
       FindPossibleMoves( aPosition );
+      ShowPossibleMoves;
+      aAllData.aMainWindow.aWindow.Show_All;
    end Activate_Button;
    
    procedure Activate_Button_Call( Object : access Gtk.Widget.Gtk_Widget_Record'Class; aPosition : in ModelLayer.Position ) is
@@ -87,6 +88,7 @@ package body ControllerLayer is
                                                    "clicked", 
                                                    UserCallback_Position.To_Marshaller( Activate_Button_Call'Access ),
                                                    aPosition ); 
+      HidePossibleMoves;
       aAllData.aMainWindow.aWindow.Show_All;
    end Deactivate_Button;
    
@@ -135,12 +137,12 @@ package body ControllerLayer is
             aPosition1, aPosition2, aPosition3, aPosition4, aPosition5, aPosition6, aPosition7 : ModelLayer.Position;
          begin
             aPosition1.aYPosition := ModelLayer.AxisY( 1 );     aPosition1.aXPosition := ModelLayer.A;
-            aPosition1.aYPosition := ModelLayer.AxisY( 2 );     aPosition1.aXPosition := ModelLayer.B;
-            aPosition1.aYPosition := ModelLayer.AxisY( 3 );     aPosition1.aXPosition := ModelLayer.C;
-            aPosition1.aYPosition := ModelLayer.AxisY( 4 );     aPosition1.aXPosition := ModelLayer.D;
-            aPosition1.aYPosition := ModelLayer.AxisY( 5 );     aPosition1.aXPosition := ModelLayer.F;
-            aPosition1.aYPosition := ModelLayer.AxisY( 6 );     aPosition1.aXPosition := ModelLayer.G;
-            aPosition1.aYPosition := ModelLayer.AxisY( 7 );     aPosition1.aXPosition := ModelLayer.H;
+            aPosition2.aYPosition := ModelLayer.AxisY( 2 );     aPosition2.aXPosition := ModelLayer.B;
+            aPosition3.aYPosition := ModelLayer.AxisY( 3 );     aPosition3.aXPosition := ModelLayer.C;
+            aPosition4.aYPosition := ModelLayer.AxisY( 4 );     aPosition4.aXPosition := ModelLayer.D;
+            aPosition5.aYPosition := ModelLayer.AxisY( 5 );     aPosition5.aXPosition := ModelLayer.E;
+            aPosition6.aYPosition := ModelLayer.AxisY( 6 );     aPosition6.aXPosition := ModelLayer.F;
+            aPosition7.aYPosition := ModelLayer.AxisY( 7 );     aPosition7.aXPosition := ModelLayer.G;
             
             aPossibleMoves := appendPossibleMoves( aPossibleMoves, aPosition1 );
             aPossibleMoves := appendPossibleMoves( aPossibleMoves, aPosition2 );
@@ -150,9 +152,8 @@ package body ControllerLayer is
             aPossibleMoves := appendPossibleMoves( aPossibleMoves, aPosition6 );
             aPossibleMoves := appendPossibleMoves( aPossibleMoves, aPosition7 );
          end;
-         Put_Line( PossibleMovesToString( aPossibleMoves ) );
+         Put_Line( "FindPossibleMoves: " & PossibleMovesToString( aPossibleMoves ) );
       end if;
-      
       
    end FindPossibleMoves;
    
@@ -212,8 +213,7 @@ package body ControllerLayer is
       --       possiblemoves.add(col+1, row-1)	
            
                
-   end FindPossibleMovesPawn;   
-      
+   end FindPossibleMovesPawn;
 
    function newPossibleMoves( outterPossibleMoves : in out PossibleMoves; newSize : in NaturalAndZero ) return PossibleMoves is
       aNewPossibleMoves : PossibleMoves;
@@ -235,6 +235,7 @@ package body ControllerLayer is
       aNewPossibleMoves : PossibleMoves; 
    begin
       aNewPossibleMoves := newPossibleMoves( aNewPossibleMoves, outterPossibleMoves.Last +1 );
+      
       if( aNewPossibleMoves.First = aNewPossibleMoves.Last ) then
          aNewPossibleMoves.aDynamicTable( aNewPossibleMoves.First ) := newPosition;
       else
@@ -243,13 +244,58 @@ package body ControllerLayer is
          end loop;
          aNewPossibleMoves.aDynamicTable( outterPossibleMoves.Last +1 ) := newPosition;
       end if;
+      
       outterPossibleMoves := aNewPossibleMoves;
       return outterPossibleMoves;
    end appendPossibleMoves;
    
+   function removePossibleMoves( outterPossibleMoves : in out PossibleMoves; aPosition : in ModelLayer.Position ) return PossibleMoves is
+      aNewPossibleMoves : PossibleMoves; 
+      J : NaturalAndZero;
+   begin
+      if( outterPossibleMoves.Last = 0 ) then
+         return outterPossibleMoves;
+      else
+         aNewPossibleMoves := newPossibleMoves( aNewPossibleMoves, outterPossibleMoves.Last -1 );
+         if( aNewPossibleMoves.Last = 0 ) then
+            outterPossibleMoves := aNewPossibleMoves;
+            return outterPossibleMoves;
+         else
+            if( aNewPossibleMoves.First = aNewPossibleMoves.Last ) then
+               for i in outterPossibleMoves.First .. outterPossibleMoves.Last loop
+                  if( outterPossibleMoves.aDynamicTable( I ).aYPosition /= aPosition.aYPosition and
+                       outterPossibleMoves.aDynamicTable( I ).aXPosition /= aPosition.aXPosition ) then
+                     aNewPossibleMoves.aDynamicTable( aNewPossibleMoves.First ) := outterPossibleMoves.aDynamicTable( I );
+                  else
+                     null; -- do nothing
+                  end if;
+               end loop;
+            else
+               J := aNewPossibleMoves.First;
+               for I in outterPossibleMoves.First .. outterPossibleMoves.Last loop
+                  if( outterPossibleMoves.aDynamicTable( I ).aYPosition /= aPosition.aYPosition and
+                       outterPossibleMoves.aDynamicTable( I ).aXPosition /= aPosition.aXPosition ) then
+                     aNewPossibleMoves.aDynamicTable( J ) := outterPossibleMoves.aDynamicTable( I );
+                     J := J + 1;
+                  else
+                     null; -- do nothing
+                  end if;
+                  exit when J > aNewPossibleMoves.Last;
+               end loop;
+            end if;
+      
+            outterPossibleMoves := aNewPossibleMoves;
+            return outterPossibleMoves;
+         end if;
+      end if;
+   end removePossibleMoves;
+   
    function PossibleMovesToString( outterPossibleMoves : in out PossibleMoves ) return String is
       aDynString : Ada.Strings.Unbounded.Unbounded_String := Ada.Strings.Unbounded.To_Unbounded_String("[ ");
    begin
+      if( outterPossibleMoves.Last = 0 ) then
+         null; -- do nothing
+         else
       if( outterPossibleMoves.First = outterPossibleMoves.Last ) then
          aDynString := aDynString & "{" & outterPossibleMoves.aDynamicTable( outterPossibleMoves.First ).aYPosition'Img & 
            "," & outterPossibleMoves.aDynamicTable( outterPossibleMoves.First ).aXPosition'Img & "}";
@@ -263,6 +309,7 @@ package body ControllerLayer is
                  "," & outterPossibleMoves.aDynamicTable( I ).aXPosition'Img & "}";
             end if;
          end loop;
+         end if;
       end if;
          
       aDynString := aDynString & " ]";
@@ -272,5 +319,70 @@ package body ControllerLayer is
          return aString;
       end;
    end;
+   
+   procedure ShowPossibleMoves is
+   begin
+      if( aPossibleMoves.Last = 0 ) then
+         null; --do nothing
+      else
+         if( aPossibleMoves.First = aPossibleMoves.Last ) then
+            ShowPossibleMove( aPossibleMoves.aDynamicTable( aPossibleMoves.Last ) );
+         else
+            for I in aPossibleMoves.First .. aPossibleMoves.Last loop
+               ShowPossibleMove( aPossibleMoves.aDynamicTable( I ) );
+            end loop;
+         end if;
+      end if;
+   end ShowPossibleMoves;
+   
+   procedure ShowPossibleMove( aPosition : in out ModelLayer.Position ) is
+   begin
+      -- Put_Line( "ShowPossibleMove [ " & aPosition.aYPosition'Img & ", " & aPosition.aXPosition'Img & " ]" );
+      aAllData.aChessBoard.aGrid( aPosition.aYPosition, aPosition.aXPosition ).isPossibleMove := True;
+      VisualLayer.Renew_Button( aRowNo      => aPosition.aYPosition,
+                                aColNo      => aPosition.aXPosition,
+                                aMainWindow => aAllData.aMainWindow,
+                                aChessBoard => aAllData.aChessBoard );
+      
+      --        IdPosition := UserCallback_Position.Connect( aAllData.aMainWindow.aButtonGrid( VisualLayer.AxisY( aPosition.aYPosition ), VisualLayer.AxisX( aPosition.aXPosition ) ), 
+      --                                                     "clicked", 
+      --                                                     UserCallback_Position.To_Marshaller( Deactivate_Button_Call'Access ),
+      --                                                     aPosition );
+   end;
+   
+   procedure HidePossibleMoves is
+   begin
+      Put_Line( "#1# HidePossibleMoves: " & PossibleMovesToString( aPossibleMoves ) );
+      if( aPossibleMoves.Last = 0 ) then
+         null; --do nothing
+      else
+         if( aPossibleMoves.First = aPossibleMoves.Last ) then
+            HidePossibleMove( aPossibleMoves.aDynamicTable( aPossibleMoves.Last ) );
+         else
+            while( aPossibleMoves.Last /= 0 ) loop
+               HidePossibleMove( aPossibleMoves.aDynamicTable( aPossibleMoves.First ) );
+            end loop;
+         end if;
+      end if;
+      Put_Line( "#2# HidePossibleMoves: " & PossibleMovesToString( aPossibleMoves ) );
+   end;
+   
+   procedure HidePossibleMove( aPosition : in out ModelLayer.Position ) is
+      IdPosition : Gtk.Handlers.Handler_Id;
+   begin
+      aAllData.aChessBoard.aGrid( aPosition.aYPosition, aPosition.aXPosition ).isPossibleMove := False;
+      VisualLayer.Renew_Button( aRowNo      => aPosition.aYPosition,
+                                aColNo      => aPosition.aXPosition,
+                                aMainWindow => aAllData.aMainWindow,
+                                aChessBoard => aAllData.aChessBoard );
+      if( aAllData.aChessBoard.aGrid( aPosition.aYPosition, aPosition.aXPosition ).isTaken = True ) then
+         IdPosition := UserCallback_Position.Connect( aAllData.aMainWindow.aButtonGrid( VisualLayer.AxisY( aPosition.aYPosition ), VisualLayer.AxisX( aPosition.aXPosition ) ), 
+                                                      "clicked", 
+                                                      UserCallback_Position.To_Marshaller( Activate_Button_Call'Access ),
+                                                      aPosition );
+      end if;
+      aPossibleMoves := removePossibleMoves( outterPossibleMoves => aPossibleMoves,
+                                             aPosition           => aPosition );
+   end HidePossibleMove;
 
 end ControllerLayer;
--- src/Prototype/controllerlayer.ads
+++ src/Prototype/controllerlayer.ads
@@ -14,7 +14,14 @@ package ControllerLayer is
    end record;
    function newPossibleMoves( outterPossibleMoves : in out PossibleMoves; newSize : in NaturalAndZero ) return PossibleMoves;
    function appendPossibleMoves( outterPossibleMoves : in out PossibleMoves; newPosition : in ModelLayer.Position ) return PossibleMoves;
+   function removePossibleMoves( outterPossibleMoves : in out PossibleMoves; aPosition : in ModelLayer.Position ) return PossibleMoves;
    function PossibleMovesToString( outterPossibleMoves : in out PossibleMoves ) return String;
+
+   procedure ShowPossibleMoves;
+   procedure ShowPossibleMove( aPosition : in out ModelLayer.Position );
+
+   procedure HidePossibleMoves;
+   procedure HidePossibleMove( aPosition : in out ModelLayer.Position );
 
    procedure SetDeactive_aActivatedPosition;
    procedure SetActive_aActivatedPosition( aPosition : in ModelLayer.Position );
